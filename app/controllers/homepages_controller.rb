a7ea4d07a97d233eab84b4b8a617b2844f8e197a
Nidhi <nidhixpatel@gmail.com>
2019-04-23 16:38:46 -0700

    main page can load even when there is no work

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,6 @@
 class HomepagesController < ApplicationController
   def index
+    @works = Work.all
    @albums = Work.top("album")
    @books = Work.top("book")
    @movies = Work.top("movies")
--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -4,4 +4,16 @@ describe HomepagesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+   it "loads main page" do
+    get root_path
+    must_respond_with :success
+   end
+
+   it "loads main page even if there are no works" do
+    Work.delete_all
+    expect(Work.count).must_equal 0
+    get root_path
+    must_respond_with :success
+   end
 end
