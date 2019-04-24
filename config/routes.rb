1b42326108601e9baf26e74aaeab2051986698d6
Nidhi <nidhixpatel@gmail.com>
2019-04-24 13:43:32 -0700

    created user model and controller, started login feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   
   resources :works
 
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # end
