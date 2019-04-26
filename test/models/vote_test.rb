2f09737e44d56ee3cc1cb51fc3ebb6e74bdead4c
Nidhi <nidhixpatel@gmail.com>
2019-04-26 15:55:28 -0700

    added model relation tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,12 +3,20 @@ require "test_helper"
 describe Vote do
   let(:work) { works(:summer)}
   let(:user) { users(:niv)}
+  let(:vote) { Vote.new(date: Date.today, work_id: work.id, user_id: user.id)}
 
   it "must be valid" do
-    vote = Vote.new(date: Date.today, work_id: work.id, user_id: user.id)
     value(vote).must_be :valid?
   end
-  
+
+  it "can get work from vote" do
+    expect(vote.work).must_equal work
+  end
+
+  it "can get user from vote" do
+    expect(vote.user).must_equal user
+  end
+
 
 end
 
